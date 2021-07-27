6a23256fec1db49f83ffaaed72e8c9fc371875f4
Praveen <praveen.kumar@crownit.in>
2021-07-27 08:27:35 +0530

    docker file update and ping service for emitter service

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:12.14.0-alpine3.9
 
 RUN mkdir /home/code
 
+#EXPOSE 3009
+
 COPY . /home/code
 
 WORKDIR /home/code
@@ -10,4 +12,8 @@ RUN npm install --production && rm -rf /var/cache/apk/*
 
 ENV NODE_ENV=production
 
+EXPOSE 3009
+
 ENTRYPOINT [ "node", "/home/code/app.js" ]
+#docker build .
+#docker run --env-file=.test-env <imageid/image name>
--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const cluster = require("cluster");
-const os = require("os");
-const dataSource = require("./data.json");
-if (false && cluster.isMaster) {
-  for (var i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", function (worker) {
-    cluster.fork();
-  });
-} else {
-  const path = require("path");
-  require("dotenv").config({ path: path.resolve(__dirname, ".env") });
-  const config = require("./config/config");
-  const { EmitterService } = require("./services/EmitterService");
-  const http = require("http").createServer();
-  const io = require("socket.io")(http);
-  let emitterServiceObj = new EmitterService();
-  setInterval(() => {
-    let randomMessageCount = Math.floor(
-      Math.random() *
-        (parseInt(config.app.MESSAGE_MAX_COUNT) -
-          parseInt(config.app.MESSAGE_MIN_COUNT) +
-          1) +
-        parseInt(config.app.MESSAGE_MIN_COUNT)
-    );
-    let _messagePacket = emitterServiceObj.generateMessageStream(
-      randomMessageCount,
-      dataSource
-    );
-    console.log(
-      `Generated ${randomMessageCount} encrypted messages.....${randomMessageCount}`
-    );
-  }, 1000 * parseInt(config.app.EMITTER_SERVICE_INTERVAL));
+const dataSource = require('./data.json');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const config = require('./config/config');
+const { EmitterService } = require('./services/EmitterService');
+const { read } = require('fs');
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+let emitterServiceObj = new EmitterService();
+setInterval(() => {
+  let randomMessageCount = Math.floor(
+    Math.random() *
+      (parseInt(config.app.MESSAGE_MAX_COUNT) -
+        parseInt(config.app.MESSAGE_MIN_COUNT) +
+        1) +
+      parseInt(config.app.MESSAGE_MIN_COUNT)
+  );
+  let _messagePacket = emitterServiceObj.generateMessageStream(
+    randomMessageCount,
+    dataSource
+  );
+  console.log(
+    `Generated ${randomMessageCount} encrypted messages.....${randomMessageCount}`
+  );
+}, 1000 * parseInt(config.app.EMITTER_SERVICE_INTERVAL));
 
-  io.on("exit", () => {
-    console.log("client disconnected");
+io.on('exit', () => {
+  console.log('client disconnected');
+});
+io.on('connection', (socket) => {
+  console.log(`New client connected`);
+  emitterServiceObj.on('data', (data) => {
+    //console.log(`Sending data to client....`);
+    socket.emit('message', data);
   });
-  io.on("connection", (socket) => {
-    console.log(`New client connected`);
-    emitterServiceObj.on("data", (data) => {
-      //console.log(`Sending data to client....`);
-      socket.emit("message", data);
-    });
-  });
-  http.listen(parseInt(config.app.EMITTER_SERVICE_PORT), () => {
-    console.log("Server Is Running Port: " + config.app.EMITTER_SERVICE_PORT);
-  });
-}
+});
+http.on("request",(req,res)=>{
+  if(req.method == "GET" && req.url == "/ping"){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify({status:1,message:"emitter service is up..."}));
+    res.end();
+  } else {
+    res.write("Emitter service is working..");
+    res.end();
+  }
+})
+http.listen(parseInt(config.app.EMITTER_SERVICE_PORT), () => {
+  console.log('Server Is Running Port: ' + config.app.EMITTER_SERVICE_PORT);
+});
